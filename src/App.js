872161f460f90e46e736947480d04e8db9ab713c
MaciejReimann <maciej.reimann@gmail.com>
2018-10-16 15:16:37 +0200

    search bar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import SearchBar from './containers/SearchBar-container'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+
+        <SearchBar />
       </div>
     );
   }
