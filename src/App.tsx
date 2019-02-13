b855ab8623ebfeb6d3c4a501dacf7da9fed073cf
MaciejReimann <maciej.reimann@gmail.com>
2019-02-13 14:42:02 +0100

    App with interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,55 @@
 import React, { Component } from "react";
 import SearchBar from "./containers/SearchBar";
 
-class App extends Component {
-  componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.error(err)
-    );
+interface State {
+  position: Position;
+  positionError: {};
+}
+
+export default class App extends Component<{}, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      position: {
+        coords: {
+          longitude: 0,
+          latitude: 0,
+          accuracy: 0,
+          altitude: 0,
+          altitudeAccuracy: 0,
+          heading: 0,
+          speed: 0
+        },
+        timestamp: 0
+      },
+      positionError: {}
+    };
   }
+
   render() {
     return (
       <div className="App">
-        {" "}
         SearchBar goes here!
         <SearchBar />
       </div>
     );
   }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      this.onSuccess,
+      this.onError,
+      options
+    );
+  }
+  onSuccess = (position: Position) => {
+    this.setState({ position });
+  };
+  onError = (positionError: PositionError) => this.setState({ positionError });
 }
 
-export default App;
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
